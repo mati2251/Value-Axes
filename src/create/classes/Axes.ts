53cc46df371aad59394ba57a74ee57d8a46a8689
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-06-05 20:19:44 +0200

    Draw Axes X

--- src/create/classes/Axes.ts
+++ src/create/classes/Axes.ts
@@ -1,11 +1,40 @@
 class Axes {
-    type: AxisType;
+    type: AxisType
     extremes: Extremes
     name: string
-    constructor(type: AxisType = AxisType.x, extremes: Extremes, name: string) {
-        this.type = type;
-        this.extremes = extremes;
+    ctx: any
+    canvas: HTMLCanvasElement
+    color: string = '#000000'
+
+    constructor(type: AxisType = AxisType.x, extremes: Extremes, name: string, canvas: HTMLCanvasElement, ctx: any) {
+        this.type = type
+        this.extremes = extremes
         this.name = name
+        this.canvas = canvas
+        this.ctx = ctx
+    }
+
+    draw = () => {
+        this.drawAxes()
+    }
+
+    drawAxes = () => {
+        this.ctx.beginPath()
+        this.ctx.lineCap = "round"
+        this.ctx.style = this.color
+        this.ctx.lineWidth = 8
+        if (this.type === AxisType.x) {
+            this.ctx.moveTo(5, this.canvas.height / 2 + 20)
+            this.ctx.lineTo(this.canvas.width-20, this.canvas.height / 2 + 20)
+            this.ctx.stroke()
+            this.ctx.closePath()
+            this.ctx.beginPath()
+            this.ctx.moveTo(this.canvas.width-20, this.canvas.height / 2 + 10)
+            this.ctx.lineTo(this.canvas.width-20, this.canvas.height / 2 + 30)
+            this.ctx.lineTo(this.canvas.width-5, this.canvas.height / 2 + 20)
+            this.ctx.fill()
+        } else if (this.type === AxisType.y) {
+        }
     }
 }
 
--- src/create/classes/Chart.ts
+++ src/create/classes/Chart.ts
@@ -18,17 +18,18 @@ class Chart {
 
     constructor(name: string, extremesX: Extremes, nameX: string, extremesY: Extremes, nameY: string, quartersValues: QuartersValues, canvas: HTMLCanvasElement) {
         this._name = name
-        this.axesX = new Axes(AxisType.x, extremesX, nameX);
-        this.axesY = new Axes(AxisType.y, extremesY, nameY)
-        this.quartersValues = quartersValues
         this.canvas = canvas
         this.ctx = this.canvas.getContext('2d')
+        this.axesX = new Axes(AxisType.x, extremesX, nameX, canvas, this.ctx);
+        this.axesY = new Axes(AxisType.y, extremesY, nameY, canvas, this.ctx)
+        this.quartersValues = quartersValues
         this.draw()
     }
 
     draw = () => {
-        this.clean()
         this.drawTitle()
+        this.axesX.draw();
+        this.axesY.draw();
     }
 
     clean = () => {
